1ebe0db4e5587cdce8ce6a436af19b1599daaa85
MasahiroTakahashi <masahiro@rightscale.com>
2012-03-27 13:48:28 +0900

    3Tier index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?
+
+include 'config/db.php';
+
+mysql_connect($hostname_DB,$username_DB,$password_DB);
+@mysql_select_db($database_DB) or die( "Unable to select database");
+$query="SELECT * FROM phptest";
+$result=mysql_query($query);
+
+$num=mysql_numrows($result);
+
+mysql_close();
+
+echo "<b><center> Database($database_DB) Output From Host($hostname_DB)</center></b><br><br>";
+
+$i=0;
+while ($i < $num) {
+
+$name=mysql_result($result,$i,0);
+$value=mysql_result($result,$i,1);
+
+
+echo "<b><center>$name:$value</center></b><br><hr><br>";
+
+$i++;
+}
+
+echo "<b><center> Starting PHPINFO:</center> </b><hr>";
+phpinfo();
+?>

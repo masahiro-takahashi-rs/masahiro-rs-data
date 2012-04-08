121a520ee1d53426b6108721fc0b28eacd8f9481
MasahiroTakahashi <masahiro@rightscale.com>
2012-04-08 09:30:03 +0900

    comment modified

--- insert.php
+++ insert.php
@@ -6,7 +6,7 @@
    
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <title>RightScale Unified Test App</title>
+    <title>Update function</title>
     <link rel="stylesheet" type="text/css" href="style.css" />  
 </head>
 
